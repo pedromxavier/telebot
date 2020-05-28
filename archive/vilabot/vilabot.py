2b6e9ae76a930a8c6612b92ebc709a36566592d6
Pedro <pedromxavier@poli.ufrj.br>
2020-05-28 19:03:40 -0300

    Update archive/vilabot

--- archive/vilabot/vilabot.py
+++ archive/vilabot/vilabot.py
@@ -1,40 +1,49 @@
 import traceback
 import lorem
+import re
+from random import randint
 
-from telegram import MessageEntity
-from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram import Message, MessageEntity
+from telegram.ext import Updater, CommandHandler, MessageHandler, BaseFilter
 
 from telebot import Bot, Filters
-from telebot.botlib import stdwar, stderr, stdout
+from telebot.proxy import Proxy, lock_start, lock_awake
+from telebot.layer import Layer, with_info
+from telebot.filters import Mention
+from telebot.botlib import log, load, stdwar, stderr, stdout
 
-with open('vilabot.token') as file:
-    TOKEN = file.read()
+TOKEN = load('vilabot.token')
+
+class MoonEmoji(BaseFilter):
+
+    MOONS = ['🌝','🌕','🌗','🌘','🌖','🌙','🌛','🌚','🌑','🌓','🌒','🌔','☪','☾','☽']
+
+    def __init__(self):
+        self.regex = re.compile('|'.join(self.MOONS), re.MULTILINE)
+
+    def filter(self, message: Message):
+        return self.regex.search(message.text) is not None
 
 class VilaBot(Bot):
 
-    START_TEXT = "Aaauuuuuuuuu!"
+    __cast__ = [with_info, lock_start, lock_awake]
 
     def get_answer(self, text: str, **kwargs):
         return text
-
-    @Bot.with_info
-    @Bot.command('start')
+        
+    @lock_start.invert
+    @Bot.command('start', 'Inicializa um chat com o Bot')
     def start(self, info):
         stdout[2] << f"> /start from @{info['username']}"
-        chat = self.get_chat(info['chat_id'])
-        if chat.started:
-            return None
-        else:
-            self.start_chat(info['chat_id'])
+        ## Starts chat
+        self.start_chat(info['chat_id'])
         kwargs = {
             'chat_id': info['chat_id'],
-            'text': self.START_TEXT,
+            'text': self.uivo,
         }
         return info['bot'].send_message(**kwargs)
 
-    @Bot.with_info
-    @Bot.lock_start
-    @Bot.command('lorem')
+    @Bot.command('lorem', 'Gera um parágrafo Lorem Ipsum')
     def lorem(self, info):
         stdout[2] << f"> /lorem from @{info['username']}"
         kwargs = {
@@ -43,20 +52,67 @@ class VilaBot(Bot):
         }
         return info['bot'].send_message(**kwargs)
 
-    @Bot.with_info
-    @Bot.lock_start
-    @Bot.message(~Filters.command, Filters.text)
-    def echo(self, info):
-        stdout[2] << f"> Text Message from @{info['username']}:\n{info['text']}"
+    @Bot.command('comandos', 'Lista os comandos disponíveis')
+    def comandos(self, info):
+        stdout[2] << f"> /comandos from @{info['username']}"
         kwargs = {
             'chat_id': info['chat_id'],
-            'text': self.get_answer(info['text']),
+            'text': "\n".join([f"/{cmd}" for cmd, des in self.commands]),
         }
         return info['bot'].send_message(**kwargs)
 
-    @Bot.with_info
-    @Bot.lock_start
-    @Bot.message(~Filters.command, Filters.text, Filters.entity('mention'))
+    @Bot.command('durma', 'Bota o VilaBot para dormir')
+    def durma(self, info):
+        stdout[2] << f"> /durma from @{info['username']}"
+        chat = self.get_chat(info['chat_id'])
+        chat.sleep()
+        kwargs = {
+            'chat_id': info['chat_id'],
+            'text': '🌙 ZZZzzzzzzz'
+        }
+        return info['bot'].send_message(**kwargs)
+
+    @lock_awake.invert
+    @Bot.command('acorde', 'Manda o VilaBot acordar')
+    def acorde(self, info):
+        stdout[2] << f"> /acorde from @{info['username']}"
+        chat = self.get_chat(info['chat_id'])
+        chat.wakeup()
+        kwargs = {
+            'chat_id': info['chat_id'],
+            'text': '☀️' + self.uivo,
+        }
+        return info['bot'].send_message(**kwargs)
+
+    @Bot.command('uive', 'Uiva o lobo')
+    def uive(self, info):
+        stdout[2] << f"> /uive from @{info['username']}"
+        kwargs = {
+            'chat_id': info['chat_id'],
+            'text': self.uivo,
+        }
+        return info['bot'].send_message(**kwargs)
+
+    @Bot.command('lista')
+    def _lista(self, info):
+        stdout[2] << f"> /lista from @{info['username']}"
+        kwargs = {
+            'chat_id': info['chat_id'],
+            'text': self.command_list,
+        }
+        return info['bot'].send_message(**kwargs)
+
+    @Bot.message(Filters.text & MoonEmoji())
+    def moon_emoji(self, info):
+        stdout[2] << f"> Text Message with Moon Emoji from @{info['username']}:\n{info['text']}"
+        kwargs = {
+            'chat_id': info['chat_id'],
+            'text': self.uivo,
+            'reply_to_message_id': info['message_id'],
+        }
+        return info['bot'].send_message(**kwargs)
+
+    @Bot.message(Mention('ovilabot'))
     def mention(self, info):
         stdout[2] << f"> Text Message from @{info['username']}:\n{info['text']}"
         kwargs = {
@@ -66,8 +122,15 @@ class VilaBot(Bot):
         }
         return info['bot'].send_message(**kwargs)
 
-    @Bot.with_info
-    @Bot.lock_start
+    @Bot.message(~Filters.command, Filters.text, ~Filters.group)
+    def echo(self, info):
+        stdout[2] << f"> Text Message from @{info['username']}:\n{info['text']}"
+        kwargs = {
+            'chat_id': info['chat_id'],
+            'text': self.get_answer(info['text']),
+        }
+        return info['bot'].send_message(**kwargs)
+
     @Bot.message(Filters.command)
     def unknown(self, info):
         stdout[2] << f"> Unknown command '{info['text']}' from @{info['username']}"
@@ -78,14 +141,16 @@ class VilaBot(Bot):
         }
         return info['bot'].send_message(**kwargs)
 
-    @Bot.with_info
-    @Bot.set_error
+    @Bot.get_error
     def error(self, info):
         for line in traceback.format_tb(info['error'].__traceback__):
             stderr << line
         stderr << info['error']
 
+    @property
+    def uivo(self):
+        return  f"{randint(1,3)*'A'}{randint(4,6)*'a'}{randint(5,8)*'u'}!"
+
 if __name__ == '__main__':
     with VilaBot(TOKEN) as bot:
         bot.run()
-        bot.clear()
